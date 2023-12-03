45a5c4c13ddd0e0fe6312a0220b610cdbc2ab409
Ryan <41839897+RyanPatty@users.noreply.github.com>
2023-12-03 03:51:27 -0500

    Add files via upload

---
+++ pandas-code-project/chunk1Sentiment.py
@@ -0,0 +1,26 @@
+#~250 words after chunked
+
+#import pandas library
+import pandas as pd
+
+#create a data object with the data from excel file in desktop filepath
+data = pd.read_excel('C:/Users/Astro/Desktop/reduce2.xlsx')
+
+data = data[data['Review'].str.contains('wish|want|help|please|broke|feature|perform|new', case=False)]
+
+#save data to a new excel file
+data.to_excel('C:/Users/Astro/Desktop/first-chunk.xlsx', index=False)
+
+#run a sentiment analysis on the data
+from textblob import TextBlob
+data['polarity'] = data['Review'].apply(lambda x: TextBlob(x).sentiment.polarity)
+data['subjectivity'] = data['Review'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+
+#save the sentiment analysis data to a new excel file
+data.to_excel('C:/Users/Astro/Desktop/first-chunk-analysis.xlsx', index=False)
+
+
+ 
+#display the data
+print(data)
+
